f8bb164c208925210988fbe4bac5e6402691b721
Messias Dias <messiasdias.ti@gmail.com>
2021-12-22 19:11:30 -0300

    test cache

--- main.js
+++ main.js
@@ -1,65 +1,89 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
+var hasPermission = ("Notification" in window) && (Notification.permission === "granted")
+var btn = document.getElementById('display')
+var notification = document.getElementById('notification')
+var offline = document.getElementById('offline')
+var installingWorker  = false
+var serviceWorker = null
+
+
+function init(){
+  if (notification) {
+     notification.addEventListener('focus', () => {
+      Notification.requestPermission(function(status) {
+        if (status === 'granted') {
+          hasPermission = true
+          if (serviceWorker === null) {
+            registerServiceWorker()
+            getServiceWorkerRegistration()
+          }
+        }
+      })
+    })
+  }
+
+  if (btn) {
+    btn.addEventListener('click', () => {
+      if(notification) {
+        displayNotification("Notificação Enviada", notification.value) 
+        notification.value = ""
+      }
+    })
+  }
+
+  registerServiceWorker()
+  getServiceWorkerRegistration()
+
+  if (!navigator.onLine) {
+    offline.innerHTML = "Ops! You Are Offline!"
+  }
+
+  if (navigator.onLine && window.location.href == "/offline") {
+    offline.innerHTML = ""
+  }
+}
+
+
+async function getServiceWorkerRegistration(){
+  if (hasPermission) return serviceWorker = await navigator.serviceWorker.getRegistration().then(reg => {return reg})
+  serviceWorker = null
+}
+
+function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    ///test-servicework/
+    navigator.serviceWorker.register('/test-servicework/sw.js')
     .then(function(registration) {
       registration.addEventListener('updatefound', function() {
-        // If updatefound is fired, it means that there's
-        // a new service worker being installed.
-        var installingWorker = registration.installing;
-        console.log('A new service worker is being installed:',
-          installingWorker);
-  
-        // You can listen for changes to the installing service worker's
-        // state via installingWorker.onstatechange
+        installingWorker = registration.installing 
       });
     })
     .catch(function(error) {
-      console.log('Service worker registration failed:', error);
+      console.error('Service worker registration failed:', error);
     });
-} else {
-    console.log('Service workers are not supported.');
-}
-
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-});
-
-function serviceWorkerRegistration(callback = (reg) => {console.log(reg)}){
-  if (Notification.permission == 'granted')  {
-    return navigator.serviceWorker.getRegistration().then(callback)
+  } else {
+    console.error('Service workers are not supported.');
   }
-  return null
 }
 
-function displayNotification(notification = null) {
-    if (Notification.permission == 'granted') {
-      serviceWorkerRegistration((reg) => {
-        console.log("SW registration:", reg)
-
-        var options = {
-          body: notification || 'Esse é o corpo da notificação de teste!',
-          icon: 'kevi.png',
-          vibrate: [100, 50, 100],
-          data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-          },
-          actions: [
-            {action: 'close', title: 'Fechar Notificação', icon: 'kevi.png'},
-          ]
-        }
-
-        reg.showNotification('Test OK!', options)
-      })
+function displayNotification(notificationTitle = null, notificationText = null) {
+  if (serviceWorker) {
+    let notificationOptions = {
+      body: notificationText != null ? notificationText:  'Esse é o corpo da notificação de teste!',
+      icon: 'kevi.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      },
+      actions: [
+        {action: 'close', title: 'Fechar Notificação', icon: 'kevi.png'},
+      ]
     }
+    serviceWorker.showNotification(notificationTitle  != null ? notificationTitle : 'Test OK!', notificationOptions)
+  } else {
+    console.error('Service workers are not supported to send notification.');
+  }
 }
 
-//displayNotification()
-
-let btn = document.getElementById('display')
-if (btn) btn.addEventListener('click', () => {
-  let notification = document.getElementById('notification')
-  if(notification) 
-    displayNotification(notification.value) 
-    notification.value = ""
-})
-
+//Run
+init()
--- sw.js
+++ sw.js
@@ -1,7 +1,52 @@
-var cacheFiles = [
-  '/kevi.png',
-  '/main.js',
+const cacheName = "v1-dev"
+const cacheFiles = [
+  '/test-servicework/',
+  'kevi.png',
+  'main.js',
+  'sw.js'
 ]
+
+const maxExecusions = 100000;
+const loopTimeout = 50000
+
+async function loop(v){
+  let now = new Date()
+
+  let request = {
+    method: "GET",
+    url: "https://servicodados.ibge.gov.br/api/v1/localidades/distritos"
+    //bodyUsed: false,
+    // cache: "default",
+    // credentials: "include",
+    // destination: "script",
+    // headers: {},
+    // integrity: "",
+    // isHistoryNavigation: false,
+    // keepalive: false,
+    // mode: "no-cors",
+    // redirect: "follow",
+    // referrer: "https://messiasdias.github.io/test-servicework/",
+    // referrerPolicy: "strict-origin-when-cross-origin",
+    // signal: {
+    //   aborted: false,
+    //   onabort: null
+    // },
+  }
+
+  let response = fetch(request)
+  console.log(response)
+  
+  console.log(`Loop version ${v} | ${now}`)
+  if (parseInt(v + 1) < maxExecusions) setTimeout(() => self.loop(v+1), loopTimeout);
+}
+
+async function  updateCache(request, response){
+  let res =  await response.then(res => res)
+  if (cacheFiles.map(path => request.url.endsWith(path)).some(p => p)) {
+    caches.open(cacheName).then(async (cache) => {cache.put(request.url, response)})
+  }
+  return res
+}
 
 self.addEventListener('notificationclose', function(e) {
   var notification = e.notification;
@@ -9,14 +54,31 @@ self.addEventListener('notificationclose', function(e) {
   console.log('Closed notification: ' + primaryKey);
 })
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open().then(function(cache) {
-      console.log('cache:', cache)
-      return cache.addAll(cacheFiles)
-      // .then(function() {
-      //   console.log('Cache Files add completed');
-      // })
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(cacheName).then(cache => {
+    return cache.addAll(cacheFiles).then(() => self.skipWaiting())
+  }))
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(cacheNames => {
+    return Promise.all(cacheNames.map(cache => {if (cache !== cacheName) return caches.delete(cache)}))
+  }))
+
+  self.loop(1)
+})
+
+self.addEventListener('fetch', event => {
+  console.log(event.request)
+  event.respondWith(
+    caches.match(event.request)
+    .then((response) => {
+      if (response) {return response}
+      self.updateCache(event.request, fetch(event.request))
+      return response
+    }).catch(error => {
+      console.log("Response error: ", error)
+      return error
     })
   )
 })
